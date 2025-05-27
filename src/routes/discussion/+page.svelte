9ab1a53318fb098a43275f2c04128c38209c8926
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2025-05-27 13:40:47 -0400

    Add discussion form
    Co-authored-by: Boden Harris <180235143+bodenharris@users.noreply.github.com>

--- src/routes/discussion/+page.svelte
+++ src/routes/discussion/+page.svelte
@@ -1,3 +1,10 @@
+<script lang="ts">
+ import type { PageData } from "./$types.js";
+ import DiscussionForm from "./discussion-form.svelte";
+ let { data }: { data: PageData } = $props();
+</script>
+ 
+<DiscussionForm {data} />
 <h1> 
     Welcome to discussions!
 </h1>
